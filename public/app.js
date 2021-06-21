7bf51fdf35e9f0e682bb4bf8cd57092cc296c5f3
Srajan  shukla <67198049+srajan15@users.noreply.github.com>
2021-06-21 18:42:36 +0530

    init commit

---
+++ public/app.js
@@ -0,0 +1,56 @@
+const weatherForm = document.querySelector('form')
+const search = document.querySelector('input')
+const msg1 = document.querySelector('#msg1')
+const msg2 = document.querySelector('#msg2')
+const msg3 = document.querySelector('#msg3')
+const msg4 = document.querySelector('#msg4')
+const msg5 = document.querySelector('#msg5')
+const msg6 = document.querySelector('#msg6')
+const msg7 = document.querySelector('#msg7')
+const img = document.querySelector('img')
+const clock = document.querySelector('#clock')
+const date = document.querySelector('#date')
+const copy = document.querySelector('#copyrightDate')
+
+weatherForm.addEventListener('submit', (e) => {
+    e.preventDefault()
+    const location = search.value
+    msg1.textContent = "Loading..."
+    msg2.textContent = ""
+
+
+
+
+    fetch("http://localhost:3000/weather?search=" + location).then((response) => {
+        response.json().then((data) => {
+            if (data.error) {
+                msg1.textContent = data.error
+                img.src = ""
+            }
+            else {
+                msg1.textContent = "weather: " + data.forecast
+                msg2.textContent = "description: " + data.description + " in " + data.address
+                msg3.textContent = "temperature: " + data.temp + "C and it\'s feels like " + data.feels + "C"
+                msg4.textContent = "humidity: " + data.humidity
+                msg5.textContent = "max temperature: " + data.max_temp + "C"
+                msg6.textContent = "latitude " + data.latitude
+                msg7.textContent = "longitude " + data.long
+                img.src = "http://openweathermap.org/img/wn/" + data.icon + "@2x.png"
+
+
+            }
+        })
+
+    })
+})
+
+setInterval(() => {
+
+    let d = new Date();
+    let t = d.toLocaleTimeString();
+    clock.textContent = t;
+    date.textContent = d.getDate() + "-" + d.getMonth() + "-" + d.getFullYear()
+    copy.textContent = d.getFullYear()
+
+
+}, 1000)
---
+++ src/index.js
@@ -0,0 +1,83 @@
+const hbs = require('hbs');
+const express = require('express');
+const path = require('path');
+const request = require('postman-request')
+
+const app = express();
+const public = path.join(__dirname, '../public')
+const hbsViews = path.join(__dirname, '../temp/views')
+const hbsTemp = path.join(__dirname, '../temp/partials')
+
+
+app.set('view engine', 'hbs')
+app.set('views', hbsViews)
+hbs.registerPartials(hbsTemp)
+
+app.use(express.static(public))
+
+app.get('/weather', (req, res) => {
+    const a = req.query
+    const query = a.search
+
+    const api = "64c4ab21ff1bfaffaf14d442e3b272b3"
+    const url = "http://api.openweathermap.org/data/2.5/weather?q=" + query + "&units=metric&appid=" + api
+
+    request({ url: url, json: true }, (error, response) => {
+        if (error) {
+            return res.send({ error: 'unable to connect' })
+        }
+        else if (response.body.cod === "404") {
+            return res.send({ error: response.body.message })
+
+
+        }
+        else if (response.body.cod === "400") {
+            return res.send({ error: response.body.message })
+        }
+        else {
+
+            return res.send({
+                forecast: response.body.weather[0].main,
+                description: response.body.weather[0].description,
+                icon: response.body.weather[0].icon,
+                address: query,
+                temp: response.body.main.temp,
+                feels: response.body.main.feels_like,
+                humidity: response.body.main.humidity,
+                max_temp: response.body.main.temp_max,
+                latitude: response.body.coord.lat,
+                long: response.body.coord.lon
+
+
+            })
+
+
+        }
+
+    })
+
+})
+
+
+
+app.get('/', (req, res) => {
+    res.render('index')
+
+
+})
+
+
+app.get('*', (req, res) => {
+    res.send('404 not found')
+})
+
+
+
+
+
+
+
+
+app.listen(3000, () => {
+    console.log('listen on 3000')
+});
