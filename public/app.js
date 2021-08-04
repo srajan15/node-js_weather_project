2066e91c7171f6f9f231c4a91b9e62a7d3dcebe6
Srajan  shukla <67198049+srajan15@users.noreply.github.com>
2021-08-04 08:10:27 +0530

    Update app.js

--- public/app.js
+++ public/app.js
@@ -2,11 +2,6 @@ const weatherForm = document.querySelector('form')
 const search = document.querySelector('input')
 const msg1 = document.querySelector('#msg1')
 const msg2 = document.querySelector('#msg2')
-const msg3 = document.querySelector('#msg3')
-const msg4 = document.querySelector('#msg4')
-const msg5 = document.querySelector('#msg5')
-const msg6 = document.querySelector('#msg6')
-const msg7 = document.querySelector('#msg7')
 const img = document.querySelector('.weatherImg')
 const clock = document.querySelector('#clock')
 const date = document.querySelector('#date')
@@ -28,13 +23,13 @@ weatherForm.addEventListener('submit', (e) => {
                 img.src = ""
             }
             else {
-                msg1.textContent = "weather: " + data.forecast
-                msg2.textContent = "description: " + data.description + " in " + data.address
-                msg3.textContent = "temperature: " + data.temp + "C and it\'s feels like " + data.feels + "C"
-                msg4.textContent = "humidity: " + data.humidity
-                msg5.textContent = "max temperature: " + data.max_temp + "C"
-                msg6.textContent = "latitude " + data.latitude
-                msg7.textContent = "longitude " + data.long
+                msg1.innerHTML = "weather: " + data.forecast +
+                "<br/> description: " + data.description + " in " + data.address +
+                "<br/> temperature: " + data.temp + "C and it\'s feels like " + data.feels + "C" +
+                "<br/> humidity: " + data.humidity +
+                "<br/> max temperature: " + data.max_temp + "C" +
+                "<br/> latitude " + data.latitude +
+                "<br/> longitude " + data.long
                 img.src = "https://openweathermap.org/img/wn/" + data.icon + "@2x.png"
 
 
